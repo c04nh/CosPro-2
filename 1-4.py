c763cf60aa1b87aef330f47331263575e2918c54
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-09-29 20:39:02 +0900

    [1차] 문제4) 등장하는 가장 많은 수와 적은수 구하기 Commit

---
+++ 1-4.py
@@ -0,0 +1,32 @@
+# 1차 문제 4
+# [1차] 문제4) 등장하는 가장 많은 수와 적은수 구하기
+
+def func_a(arr):
+    counter = [0 for _ in range(1001)]
+    for x in arr:
+        counter[x] += 1
+    return counter
+
+
+def func_b(arr):
+    ret = 0
+    for x in arr:
+        if ret < x:
+            ret = x
+    return ret
+
+
+def func_c(arr):
+    INF = 1001
+    ret = INF
+    for x in arr:
+        if x != 0 and ret > x:
+            ret = x
+    return ret
+
+
+def solution(arr):
+    counter = func_a(arr)
+    max_cnt = func_b(counter)
+    min_cnt = func_c(counter)
+    return max_cnt // min_cnt
