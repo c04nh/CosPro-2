9564a8cc036ea4d2f591d7c9388b891b39ce2f8a
c04nh <w2038@e-mirim.hs.kr>
2022-01-10 09:19:13 +0900

    [4차] 문제8) 누가 반장이 될까요? Commit

---
+++ 4-08.py
@@ -0,0 +1,28 @@
+# 4차 문제 9
+# [4차] 문제8) 누가 반장이 될까요?
+
+def solution(n, votes):
+	answer = 0
+	votes_len = len(votes)
+	candidate = votes[0]
+	count = 1
+	for i in range (1, votes_len) :
+		if candidate == votes[i] :
+			count += 1
+		else :
+			count -= 1
+		if count == 0 :
+			candidate = votes[i]
+			count = 1
+
+	test_count = 0
+	for i in range(0, votes_len) :
+		if votes[i] == candidate :
+			test_count += 1
+
+	if test_count > votes_len // 2 :
+		answer = candidate
+	else :
+		answer = -1
+
+	return answer
